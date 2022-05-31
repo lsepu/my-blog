0264c19d2944000850005de986f8596ccf0a10db
santiagoposadag <santiago90dell@gmail.com>
2022-05-31 05:57:33 -0500

    created the base for the reducer

---
+++ src/state/reducer.ts
@@ -0,0 +1,49 @@
+export type commentType = {
+  comment:string,
+  author:string,
+}
+
+export type postType = {
+  title:string,
+  content:string,
+  comments: commentType[]
+}
+
+export type stateType = {
+  logged:boolean,
+  posts:postType[],
+}
+
+export enum actionsEnum{
+  toggle_log = 'TOGGLE_LOG',
+  add_post = 'ADD_POST',
+  delete_post = 'DELETE_POST',
+  add_comment = 'ADD_COMMENT',
+  delete_comment = 'DELETE_COMMENT',  
+}
+
+export type actionType = {
+  type:actionsEnum,
+  payload:any
+} 
+
+export interface reducerFunction {
+  (state:stateType, action:actionType):stateType | Error
+}
+
+export const reducer:reducerFunction = (state:stateType, action:actionType):stateType | Error => {
+  switch(action.type){
+    case actionsEnum.toggle_log:
+      return state
+    case actionsEnum.add_post:
+      return state
+    case actionsEnum.delete_post:
+      return state
+    case actionsEnum.add_post:
+      return state
+    case actionsEnum.delete_post:
+      return state
+    default:
+      return new Error("Any of the actions in the state manager were matched") 
+  }
+}
